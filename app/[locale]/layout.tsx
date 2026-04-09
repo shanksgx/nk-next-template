6b94018ac1ce641f966dac0ea460d1ce5d1d15f2
goduer <gooddidi@126.com>
2026-04-09 19:22:49 +0800

    fix: 重新引入分分析器

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -8,7 +8,7 @@ import type { ReactNode } from 'react'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { Metadata } from 'next'
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 import '../globals.css'
 
 export const metadata: Metadata = {
